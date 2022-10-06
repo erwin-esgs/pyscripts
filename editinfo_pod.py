c4051a5828b99951d083930793cb80e2c838142e
ertanto123 <ertanto.422020003@civitas.ukrida.ac.id>
2022-10-06 16:23:52 +0700

    edit if code numeric

--- editinfo_pod.py
+++ editinfo_pod.py
@@ -10,23 +10,24 @@ def main():
     df["Simcard Number"] = df["Simcard Number"].str.slice(start=0, stop=19)
     
     for index, row in df.iterrows():
-        if(index >= 3908 and index <= 3968):
-            print(str(index)+"========================================================================")
-            headers={'x-access-token': os.getenv('X_ACCESS_TOKEN')}
-            
-            name = row["Code"]+'_'+row["IMEI"]
-            URL = 'https://api.podiotsuite.com/v3/assets/'+row["Simcard Number"]
-            r = requests.put('https://api.podiotsuite.com/v3/assets/'+row["Simcard Number"] , headers=headers, data={"name": name,"accountId": "364547dc-73dc-51a0-b9ee-8e7ea8a20733"}).json()
-            print(r)
-            time.sleep(0.1)
-            URL = URL+'/groupname'
-            groupname = row["Allocation"]
-            if(groupname == "DKI Jakarta"):
-                groupname = "DKI"
-            r = requests.put('https://api.podiotsuite.com/v3/assets/'+row["Simcard Number"]+"/groupname" , headers=headers, data={"groupname": groupname,"accountId": "364547dc-73dc-51a0-b9ee-8e7ea8a20733"}).json()
-            print(r)
-            print("========================================================================")
-            time.sleep(0.1)
+        if(str(row["Code"]).isnumeric()):
+            if( int(row["Code"]) >= 2375 and int(row["Code"]) <= 2500):
+                print(str(index)+"_"+row["Code"]+"========================================================================")
+                headers={'x-access-token': os.getenv('X_ACCESS_TOKEN')}
+                
+                name = row["Code"]+'_'+row["IMEI"]
+                URL = 'https://api.podiotsuite.com/v3/assets/'+row["Simcard Number"]
+                r = requests.put('https://api.podiotsuite.com/v3/assets/'+row["Simcard Number"] , headers=headers, data={"name": name,"accountId": "364547dc-73dc-51a0-b9ee-8e7ea8a20733"}).json()
+                print(r)
+                time.sleep(0.1)
+                URL = URL+'/groupname'
+                groupname = row["Allocation"]
+                if(groupname == "DKI Jakarta"):
+                    groupname = "DKI"
+                r = requests.put('https://api.podiotsuite.com/v3/assets/'+row["Simcard Number"]+"/groupname" , headers=headers, data={"groupname": groupname,"accountId": "364547dc-73dc-51a0-b9ee-8e7ea8a20733"}).json()
+                print(r)
+                print("========================================================================")
+                time.sleep(0.1)
 
     # <Code>_<IMEI>
 if __name__ == "__main__":
